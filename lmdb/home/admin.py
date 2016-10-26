ae0ee96649e7a75a29626ff6de630a7be7f865fb
huzaifa <huzaifafaruqui@outlook.com>
2016-10-26 23:20:15 +0530

    Updated models & admin interface

--- lmdb/home/admin.py
+++ lmdb/home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from home.models import Movies,Actor,Director
+
+class MoviesAdmin(admin.ModelAdmin):
+    list_display=('title','release_date','duration','director')
+    search_fields=('title','release_date')
+    list_filter=('release_date',)
+    date_hierarchy=('release_date')
+    ordering=('-release_date',)
+admin.site.register(Movies,MoviesAdmin)
+admin.site.register(Director)
+admin.site.register(Actor)
--- lmdb/home/models.py
+++ lmdb/home/models.py
@@ -4,13 +4,26 @@ from django.db import models
 
 # Create your models here.
 
+class Actor(models.Model):
+	first_name=models.CharField(max_length=30,default='huzaifa')
+	last_name=models.CharField(max_length=40,default='faruqui')
+	age=models.IntegerField(default='20')
+	def __str__(self):
+		return u'%s %s' % (self.first_name, self.last_name)
+	
+class Director(models.Model):
+	first_name=models.CharField(max_length=30,default='huzaifa')
+	last_name=models.CharField(max_length=40,default='faruqui')
+	def __str__(self):
+		return u'%s %s' % (self.first_name, self.last_name)
 
-    
-class movies(models.Model):
+class Movies(models.Model):
 	title=models.CharField(max_length=30)
 	release_date=models.DateField()
+	actors=models.ManyToManyField(Actor)
+	director=models.ForeignKey(Director)
 	image=models.ImageField(upload_to = 'pic_folder/', default = 'pictures/movie2.jpg')
-	movie_id=models.IntegerField()
 	duration=models.DurationField()
-
+	def __str__(self):
+		return u'%s' % (self.title)
 
